3b5b5d69957f2c6f7f918d2ecea8a8a3d6ead0a5
fbock <fbock@student.42heilbronn.de>
2024-02-05 12:07:15 +0100

    alias commit

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.h                                             :+:      :+:    :+:   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 11:38:21 by ***REMOVED***             #+#    #+#             */
-/*   Updated: 2024/02/05 11:38:41 by ***REMOVED***            ###   ########.fr       */
+/*   Updated: 2024/02/05 12:07:14 by ***REMOVED***            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// check when parsing if map is too big, throw error.
+#define MAX_MAP_X_WIDTH 	1000
+#define MAX_MAP_Y_HEIGHT	1000
+
+typedef enum e_field {
+	EMPTY,
+	WALL,
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST,
+	NONE  // initialize array with none
+} t_field;
+
+typedef struct s_map {
+	size_t	size_x;
+	size_t	size_y;
+	t_field	map[MAX_MAP_Y_HEIGHT][MAX_MAP_X_WIDTH]  //
+}	t_map;
