f04d8ec93eb98b9996337527d07d61f5b591ed19
fbock <fbock@student.42heilbronn.de>
2024-02-05 12:12:52 +0100

    alias commit

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: fbock <fbock@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 11:38:21 by fbock             #+#    #+#             */
-/*   Updated: 2024/02/05 12:12:15 by fbock            ###   ########.fr       */
+/*   Updated: 2024/02/05 12:12:51 by fbock            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,5 @@ typedef enum e_field {
 typedef struct s_map {
 	size_t	amount_y_rows;		// amount of actual used y rows
 	size_t	amount_x_rows[MAX_MAP_Y_HEIGHT];  // stores the x length of the current y row
-	t_field	map[MAX_MAP_Y_HEIGHT][MAX_MAP_X_WIDTH]
+	t_field	map[MAX_MAP_Y_HEIGHT][MAX_MAP_X_WIDTH]  // contains the kind of map element (enum) at Y/X
 }	t_map;
