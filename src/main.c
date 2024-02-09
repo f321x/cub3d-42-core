12c50f00ba226dc5369ace2771fc496bd1920b0b
fbock <fbock@student.42heilbronn.de>
2024-02-09 14:34:00 +0100

    alias commit

--- src/main.c
+++ src/main.c
@@ -6,11 +6,17 @@
 /*   By: fbock <fbock@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 11:38:07 by fbock             #+#    #+#             */
-/*   Updated: 2024/02/09 13:54:20 by fbock            ###   ########.fr       */
+/*   Updated: 2024/02/09 14:33:59 by fbock            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void	end_program(t_conf_file *conf_file, t_window_frame *gui)
+{
+	mlx_terminate(gui.window);
+	free_config_file_members(&config_file);
+}
 
 int	main(int argc, char **argv)
 {
@@ -23,6 +29,9 @@ int	main(int argc, char **argv)
 
 	if (!parse_config_file(argc, argv, &config_file))
 		return (config_file.error);
-	free_config_file_members(&config_file);
+
+	mlx_image_to_window(gui.window, gui.frame, 0, 0);
+	mlx_loop(gui.window);
+	end_program(&config_file, &gui);
 	return (0);
 }
