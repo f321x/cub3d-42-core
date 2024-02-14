100ccd6520f6345e2d7ef6f3547735fb55a4f5b3
fbock <fbock@student.42heilbronn.de>
2024-02-14 18:06:31 +0100

    alias commit

--- src/raycasting/raycasting_main.c
+++ src/raycasting/raycasting_main.c
@@ -6,7 +6,7 @@
 /*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 14:47:40 by ***REMOVED***             #+#    #+#             */
-/*   Updated: 2024/02/14 15:23:59 by ***REMOVED***            ###   ########.fr       */
+/*   Updated: 2024/02/14 18:06:30 by ***REMOVED***            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ typedef struct s_player_pos {
 	double	player_pos_y;
 	double	player_dir_x;
 	double	player_dir_y;
+	int		map_x;
+	int		map_y;
 }	t_player_pos;
 
 typedef struct s_ray_steps {
@@ -73,15 +75,55 @@ t_ray	*calculate_rays(int frame_width, t_player_pos current_player_pos)
 	return (frame_rays);  // heap, has to be freed!, NULL check needed!
 }
 
-// calculates the distance for the ray from one square wall to
+// calculates the distance for the ray from one square side to
 // another on x and y axis.
 // we can use 1 instead of len of ray dir X/Y, because only the *ratio* between
 // delta_dist_x and delta_dist_y matters for the DDA.
-t_ray_steps	calc_ray_step_distances(double ray_dir_x, double ray_dir_y)
+t_ray_steps	calc_ray_step_distances(t_ray ray)
 {
 	t_ray_steps	ray_step_distance;
 
 	ray_step_distance.delta_dist_x = fabs(1 / ray_dir_x);
 	ray_step_distance.delta_dist_y = fabs(1 / ray_dir_y);
 	return (ray_step_distance);
+}
+
+// the doubles are the distance from the point to the first side of a square
+// the ints are the direction of the first step
+typedef struct s_init_step {
+	double	init_dist_x;
+	double	init_dist_y;
+	int		step_dir_x;
+	int		step_dir_y;
+}	t_init_step;
+
+// calculates the direction of the initial step for the dda
+// also calculates the initial distance from the ray point in the first
+// square to the first side of a square.
+t_init_step	calculate_initial_step_and_dist(t_ray ray, t_ray_steps steps,
+											t_player_pos p)
+{
+	t_init_step	init_dda_step;
+
+	if (ray.ray_dir_x < 0)
+	{
+		init_dda_step.step_dir_x = -1;
+		init_dda_step.init_dist_x = (p.player_pos_x - p.map_x) * ray.delta_dist_x;
+	}
+	else
+	{
+		init_dda_step.step_dir_x = 1;
+		init_dda_step.init_dist_x = (p.map_x + 1.0 - p.player_pos_x) * ray.delta_dist_x ;
+	}
+	if (ray.ray_dir_y < 0)
+	{
+		init_dda_step.step_dir_y = -1;
+		init_dda_step.init_dist_y = (p.player_pos_y - p.map_y) * ray.delta_dist_y;
+	}
+	else
+	{
+		init_dda_step.step_dir_y = 1;
+		init_dda_step.init_dist_y = (p.map_y + 1.0 - p.player_pos_y) * ray.delta_dist_y;
+	}
+	return (init_dda_step);
 }
