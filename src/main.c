43593774828a63c364298a45965b57e953c1a173
fbock <fbock@student.42heilbronn.de>
2024-03-18 11:15:46 +0100

    alias commit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ***REMOVED*** <***REMOVED***@student.***REMOVED***.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 11:38:07 by ***REMOVED***             #+#    #+#             */
-/*   Updated: 2024/03/18 10:45:23 by ***REMOVED***            ###   ########.fr       */
+/*   Updated: 2024/03/18 11:11:47 by ***REMOVED***            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,13 @@ static void	end_program(t_conf_file *conf_file, t_window_frame *gui)
 	free_config_file_members(conf_file);
 }
 
+static void	set_player_direction(t_window_frame *gui)
+{
+
+	p->player_dir_x = -1;
+	p->player_dir_y = 0;
+}
+
 // initializes the player position in the gui struct
 // with the parsed start coordinates from the map file
 static void	init_player(t_window_frame *gui)
@@ -27,8 +34,7 @@ static void	init_player(t_window_frame *gui)
 	p = &(gui->player);
 	p->player_pos_x = (double)gui->config_file.map->player_coord[0];
 	p->player_pos_y = (double)gui->config_file.map->player_coord[1];
-	p->player_dir_x = -1;
-	p->player_dir_y = 0;
+	set_player_direction(gui);
 	p->camera_plane_x = 0;
 	p->camera_plane_y = 0.66;
 	p->map_x = gui->config_file.map->player_coord[0];
